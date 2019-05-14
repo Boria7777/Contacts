e4eb833f4a7767ee1a32f2caa863d7d59d5322a7
baoyong@royasoft.com.cn <by5159226>
2019-05-14 17:54:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/contacts/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.contacts;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import com.example.contacts.fragment.addContactsFragment;
+import com.example.contacts.fragment.contactsFragment;
+import com.example.contacts.fragment.settingFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+    BottomNavigationView.OnNavigationItemSelectedListener {
+
+  private BottomNavigationView navigation;
+  private Fragment contactsFragment = new contactsFragment();
+  private Fragment addContactsFragment = new addContactsFragment();
+  private Fragment settingFragment = new settingFragment();
+  private Fragment currentFragment;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    getSupportActionBar().setTitle(R.string.title_home);
+    initView();
+    initListener();
+    switchFragment(contactsFragment).commit();
+  }
+
+  private void initView() {
+    navigation = findViewById(R.id.navigation);
+  }
+
+  private void initListener() {
+    navigation.setOnNavigationItemSelectedListener(this);
+  }
+
+  private FragmentTransaction switchFragment(Fragment targetFragment) {
+    FragmentTransaction transaction = this.getFragmentManager().beginTransaction();
+    if (!targetFragment.isAdded()) {
+      if (currentFragment != null) {
+        transaction.hide(currentFragment);
+      }
+      transaction.add(R.id.video_FrameLayout, targetFragment, targetFragment.getClass().getName());
+    } else {
+      transaction.hide(currentFragment).show(targetFragment);
+    }
+    currentFragment = targetFragment;
+    return transaction;
+  }
+
+  @Override public void onClick(View v) {
+
+  }
+
+  @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.navigation_home:
+        getSupportActionBar().setTitle(R.string.title_home);
+        switchFragment(contactsFragment).commit();
+        return true;
+      case R.id.navigation_dashboard:
+        getSupportActionBar().setTitle(R.string.title_dashboard);
+        switchFragment(addContactsFragment).commit();
+        return true;
+      case R.id.navigation_notifications:
+        getSupportActionBar().setTitle(R.string.title_notifications);
+        switchFragment(settingFragment).commit();
+        return true;
+    }
+    return false;
+  }
+}
---
+++ app/src/main/java/com/example/contacts/fragment/addContactsFragment.java
@@ -0,0 +1,96 @@
+package com.example.contacts.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.contacts.R;
+
+public class addContactsFragment extends Fragment {
+  private RecyclerView mRecyclerView;
+
+
+  @Nullable @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+      Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.addcontacts_fragment, container, false);
+    return view;
+  }
+
+  //private void initAdapter() {
+  //  adapter = new MeetingListAdapter(getActivity());
+  //  adapter.setOnclickListener(new BaseAdapter.onClickListener<MeetingListBean>() {
+  //    @Override public void onClick(MeetingListBean bean) {
+  //
+  //      String json = JSONObject.toJSONString(bean);
+  //      Intent intent = new Intent(getActivity(), VideoReserveActivity.class);
+  //      intent.putExtra("type", 3);
+  //      intent.putExtra("MeetingListBean", json);
+  //      startActivityForResult(intent, 11);
+  //    }
+  //  });
+  //  mAdapter = new LoadMoreAdapterWrapper(adapter, new LoadMoreAdapterWrapper.OnLoad() {
+  //    @Override public void load() {
+  //      loadMoreData();
+  //    }
+  //  });
+  //  mRecyclerView.setAdapter(mAdapter);
+  //  mRecyclerView.setLayoutManager(
+  //      new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+  //}
+  //
+  //private synchronized void loadMoreData() {
+  //  MeetingHttpUtil.getInstance()
+  //      .GetMeetingList(getActivity(), pagePosition, 2, new MeetingHttpUtil.httpListener() {
+  //        @Override public void onSuccess(Object jsonObject) {
+  //
+  //          JSONObject bean = ((JSONObject) jsonObject);
+  //          List<MeetingListBean> list =
+  //              JSONObject.parseArray(bean.getJSONArray("data").toJSONString(),
+  //                  MeetingListBean.class);
+  //          addContactsList(list);
+  //          showHasMore(pagePosition <= (int) bean.get("nums"));
+  //          swiperereshlayout.setRefreshing(false);
+  //          showNodata((list == null || list.size() == 0) && pagePosition == 1);
+  //          pagePosition++;
+  //
+  //        }
+  //
+  //        @Override public void onFailure() {
+  //          System.out.println(2);
+  //        }
+  //      });
+  //}
+  //
+  //private void showNodata(final boolean state) {
+  //  getActivity().runOnUiThread(new Runnable() {
+  //    @Override public void run() {
+  //      if (state) {
+  //        mRecyclerView.setVisibility(View.GONE);
+  //        chkNoVideo.setVisibility(View.VISIBLE);
+  //      } else {
+  //        mRecyclerView.setVisibility(View.VISIBLE);
+  //        chkNoVideo.setVisibility(View.GONE);
+  //      }
+  //    }
+  //  });
+  //}
+  //public void addContactsList(final List<MeetingListBean> list) {
+  //  getActivity().runOnUiThread(new Runnable() {
+  //    @Override public void run() {
+  //      adapter.appendData(list);
+  //    }
+  //  });
+  //}
+  //
+  //public void showHasMore(final boolean state) {
+  //  getActivity().runOnUiThread(new Runnable() {
+  //    @Override public void run() {
+  //      ((LoadMoreAdapterWrapper) mAdapter).setHasMoreData(state);
+  //    }
+  //  });
+  //}
+}
